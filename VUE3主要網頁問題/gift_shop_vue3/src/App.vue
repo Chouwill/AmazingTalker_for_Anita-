22bdd5bb8ebb3ae719f90bafa22e3b4efe8c52e0
Chouwill <will92068@gmail.com>
2024-01-10 09:53:36 +0800

    更新補gift_shop套VUE3

---
+++ VUE3主要網頁問題/gift_shop_vue3/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrap">
+    <Header />
+  </div>
+</template>
+
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
+<style scoped>
+ * {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        text-decoration: none;
+        box-sizing: border-box;
+      }
+      .wrap {
+        max-width: 1200px;
+        border: 2px solid yellowgreen;
+        margin: 0 auto;
+      }
+
+
+      /* 問題提問
+         1.目前專案導入VUE3都會跑版
+         2.是否要在網頁再包一層.wrap
+         3.https://zh.wix.com/website-template/view/html/3212?originUrl=https%3A%2F%2Fzh.wix.com%2Fwebsite%2Ftemplates&tpClick=view_button&esi=b3c20a59-dda0-4641-bf36-dee166ca72c6 是否要再包一層.wrap ，寬度要設多少
+        4.           */
+
+</style>
---
+++ VUE3主要網頁問題/gift_shop_vue3/src/components/Header.vue
@@ -0,0 +1,93 @@
+<template>
+  <header>
+    <div class="search_box">
+      <input type="search" value="Search" />
+    </div>
+    <nav>
+      <div class="nav_logo">
+        <a href="#"><h1>JSY</h1></a>
+      </div>
+      <div class="menu">
+        <a href="#">HOME & LiVING</a>
+        <a href="#">Accessories</a>
+        <a href="#">Stationery</a>
+        <a href="#">Sale</a>
+        <a href="#">Gift Card</a>
+      </div>
+    </nav>
+    <ul class="user">
+      <li>
+        <a href="#"><i class="fa-solid fa-user"></i></a>
+        <h4>Log In</h4>
+      </li>
+      <li>
+        <h4>Cart (0)</h4>
+      </li>
+    </ul>
+    <!-- <img src="https://static.wixstatic.com/media/c837a6_327e6bf04c1c4443ad6c4f13728c41fb~mv2.jpg/v1/fill/w_1903,h_511,al_r,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_327e6bf04c1c4443ad6c4f13728c41fb~mv2.jpg" alt=""> -->
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped>
+header {
+  display: grid;
+
+  width: 100%;
+  grid-template-columns: repeat(5, 240px);
+  grid-template-rows: repeat(3, 50px);
+  grid-gap: 10px;
+  background-color: yellowgreen;
+  box-sizing: border-box;
+}
+.search_box {
+  line-height: 3;
+  background-color: aqua;
+  grid-row-start: 2;
+  grid-row-end: 3;
+}
+nav {
+  width: 550px;
+  text-align: center;
+  background-color: orangered;
+  grid-column-start: 2;
+  grid-column-end: 3;
+  grid-row-start: 2;
+  grid-row-end: 3;
+  margin-left: 50px;
+}
+.nav_logo {
+  line-height: 2;
+}
+.menu {
+  width: 100%;
+  margin: 15px 0;
+  background-color: burlywood;
+}
+.menu a {
+  margin: 0 15px;
+}
+.user {
+  width: 205px;
+  background-color: palegreen;
+  display: flex;
+  grid-column-start: 5;
+  grid-column-end: 5;
+  grid-row-start: 2;
+  grid-row-end: 3;
+}
+.user li:first-child {
+  display: flex;
+  justify-content: space-around;
+  flex: 2;
+  /* border: 2px solid red; */
+}
+.user li:last-child {
+  flex: 1;
+}
+.user a,
+h4 {
+  font-size: 20px;
+}
+</style>
